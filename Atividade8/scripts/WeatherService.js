bc27f324b9e939d0bc50228796bb7236aa8508b7
Luis Felipe <luisfelipealves128@gmail.com>
2024-04-21 22:49:55 -0300

    arrumando erros

--- Atividade8/scripts/WeatherService.js
+++ Atividade8/scripts/WeatherService.js
@@ -5,6 +5,11 @@ class WeatherService {
             const response = await data.json();
             const dados = new WeatherModel();
             const view = new WeatherView();
+            dados.temp = response.main.temp
+            dados.city = response.name
+            dados.description = response.weather.description
+            dados.temp_min = response.main.temp_min
+            dados.temp_max = response.main.temp_max
             view.showWeather(response);
             console.log(response);
             return dados;
@@ -18,6 +23,12 @@ class WeatherService {
             const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=new%20york&appid=c05e646600469c338e44738a1fb42d3e&units=metric`);
             const response = await data.json();
             const view = new WeatherView();
+            const dados = new WeatherModel();
+            dados.temp = response.main.temp
+            dados.city = response.name
+            dados.description = response.weather.description
+            dados.temp_min = response.main.temp_min
+            dados.temp_max = response.main.temp_max
             view.showNewYork(response);
             console.log(response);
             return dados;
@@ -31,6 +42,12 @@ class WeatherService {
             const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=c05e646600469c338e44738a1fb42d3e&units=metric`);
             const response = await data.json();
             const view = new WeatherView();
+            const dados = new WeatherModel();
+            dados.temp = response.main.temp
+            dados.city = response.name
+            dados.description = response.weather.description
+            dados.temp_min = response.main.temp_min
+            dados.temp_max = response.main.temp_max
             view.showLondon(response);
             console.log(response);
             return dados;
@@ -44,6 +61,31 @@ class WeatherService {
             const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=paris&appid=c05e646600469c338e44738a1fb42d3e&units=metric`);
             const response = await data.json();
             const view = new WeatherView();
+            const dados = new WeatherModel();
+            dados.temp = response.main.temp
+            dados.city = response.name
+            dados.description = response.weather.description
+            dados.temp_min = response.main.temp_min
+            dados.temp_max = response.main.temp_max
+            view.showParis(response);
+            console.log(response);
+            return dados;
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    async requestSP() {
+        try {
+            const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=sao%20paulo&appid=c05e646600469c338e44738a1fb42d3e&units=metric`);
+            const response = await data.json();
+            const view = new WeatherView();
+            const dados = new WeatherModel();
+            dados.temp = response.main.temp
+            dados.city = response.name
+            dados.description = response.weather.description
+            dados.temp_min = response.main.temp_min
+            dados.temp_max = response.main.temp_max
             view.showParis(response);
             console.log(response);
             return dados;
